128adf96a1c5053efee606c3631bf08e1cdb0199
Huabuxiu <1347638343@qq.com>
2017-09-19 09:40:50 -0700

    基本功能

---
+++ game_functions.py
@@ -0,0 +1,116 @@
+import sys
+
+import pygame
+
+from bullet import  Bullet
+
+from alien import Alien
+
+def check_keydown_events(event,ai_settings,screen,ship,bullets):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    """如果没有到达限制就发一颗子弹出去"""
+    #创建一个新子弹，然后放入bullets组中去
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ai_settings, screen,ship,bullets):
+        #监视键盘和鼠标事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+def update_screen (ai_settings,screen,ship,aliens,bullets):
+    """更新屏幕上的像"""
+    screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    ship.blitme()
+    aliens.draw(screen)
+    #让最近绘制的屏幕可见
+    pygame.display.flip()
+
+def get_number_aliens_x(ai_settings,alien_width):
+    """计算每行可以容纳多少个外星人"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def get_number_aliens_row(ai_settings,ship_height,alien_height):
+    """计算可容纳多少行外星人"""
+    available_space_y = ai_settings.screen_height - (3 * alien_height) - ship_height
+    number_aliens_rows = int(available_space_y / (2 * alien_height))
+    return number_aliens_rows
+
+def create_alien(ai_settings,screen,aliens,alien_number,row_number):
+    """创建一个外形人并放在当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+
+
+def create_fleet(ai_settings,screen,ship,aliens):
+    """创建外星人群"""
+    #创建一个外星人，并计算一行可容纳多少外星人
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_aliens_row(ai_settings,ship.rect.height,alien.rect.height)
+    #创建第一行外星人
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            #创建一个外星人并将其加入当前行
+            create_alien(ai_settings,screen,aliens,alien_number,row_number)
+
+def update_bullets(aliens,bullets):
+    """更新子弹的位置，并删除已经消失的子弹"""
+    bullets.update()
+    #检查是否有子弹击中了外星人
+    #如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
+
+
+
+
+
+def update_aliens(ai_settings,aliens):
+    check_fleet_edges(ai_settings,aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings,aliens):
+    """如外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings,aliens)
+            break
+
+def change_fleet_direction(ai_settings,aliens):
+    """将正群外星人下移并改变他们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
